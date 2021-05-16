e6bf69007bdd3b00b1c1ac7d3096e90953e1b735
dev-stan <67323109+dev-stan@users.noreply.github.com>
2021-05-16 15:20:56 +0200

    Fix block rotation and added deduct 1 block from inv

--- src/dev/stan/mc/listeners/ClickEvent.java
+++ src/dev/stan/mc/listeners/ClickEvent.java
@@ -5,93 +5,53 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.block.data.Directional;
 
 public class ClickEvent implements Listener {
-	
-
 
 	@EventHandler
 	public void onClick(PlayerInteractEvent event) {
 
 		Player player = event.getPlayer();
-		
-		if (!player.isSneaking()) {
+		Material mat = player.getInventory().getItemInMainHand().getType();
+
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+
+			// Check if player has repair block in their main hand (mat)
 			
-			// Get main hand item
+			if(mat == Material.IRON_BLOCK || mat == Material.ANVIL || mat == Material.CHIPPED_ANVIL || mat == Material.DAMAGED_ANVIL)  {
 
-			Material mainHand = player.getInventory().getItemInMainHand().getType();
+				// Get block and blockdata player is pointing at
+				Block b = player.getTargetBlock(null, 5);
+				Location loc = b.getLocation();
+				
+				BlockData data = b.getBlockData();
+				BlockFace face = ((Directional) data).getFacing();
 
-			if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-
-				// Check if player has an iron ingot in their hand
-
-				if (mainHand == Material.IRON_BLOCK || mainHand == Material.CHIPPED_ANVIL || mainHand == Material.DAMAGED_ANVIL) {
-
-					// Get block player is pointing at
-
-					Block b = player.getTargetBlock(null, 5);
-					Location loc = b.getLocation();
-					BlockData data = b.getBlockData();
-					BlockFace facing = ((Directional) data).getFacing();
-					
-
-					
-					// Check if the player is looking at an anvil
-
-					if (b.getType().equals(Material.CHIPPED_ANVIL) || b.getType().equals(Material.DAMAGED_ANVIL)) { 
+				if (b.getType() == Material.CHIPPED_ANVIL || b.getType() == Material.DAMAGED_ANVIL) {
+				
+					if (data instanceof Directional) {
 						
-						// Set new block (with new block data)
-						loc.getBlock().setType(Material.ANVIL);
+						loc.getBlock().setType(Material.ANVIL); // Set anvil in target block
 						
-						// Get block data
+						// Get data for the new block
 						b = player.getTargetBlock(null, 5);
 						data = b.getBlockData();
 						
 						// Modify and set block data
-						((Directional) data).setFacing(facing);
+						((Directional) data).setFacing(face);
 						b.setBlockData(data);
 						
-						
-						
-
-						
-						/*Block NewB = player.getTargetBlock(null, 5);
-						
-						
-						System.out.println("Block set to:" + NewB.getType().toString());
-						System.out.println("Location: " + NewB.getLocation().toString());
-						System.out.println("old_blocl: " + b.getType().toString());
-						
-						// Fix anvil direction to mainHandch the old one
-
-						BlockFace face =((Directional) blockData).getFacing();
-						if (blockData instanceof Directional) {
-						  ((Directional) blockData).setFacing(face);
-						  NewB.setBlockData((Directional)blockData);
-						  
-						  
-						  System.out.println("Anvil direction changed to: " + face.toString());
-						} */
-						
-						player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount()-1); // This nice function clears 1 item in the main hand
-						
-						event.setCancelled(true);
 					}
-
+					player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount()-1); // This nice function clears 1 item in the main hand
+					event.setCancelled(true);
 				}
-		} // Permission bracket
-			
-		}
-		
-		else {
-			System.out.println("Event cancelled because of sneaking for " + player.getDisplayName());
+			}
 		}
 	}
-	
 }
