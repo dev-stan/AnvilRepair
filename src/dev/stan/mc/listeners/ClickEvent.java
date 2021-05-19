7aacb60564b60e322fe1e7d96cdd6985585ed616
dev-stan <67323109+dev-stan@users.noreply.github.com>
2021-05-19 20:36:42 +0200

    Clean up code, fix npe

--- src/dev/stan/mc/listeners/ClickEvent.java
+++ src/dev/stan/mc/listeners/ClickEvent.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Particle.DustOptions;
+//import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
@@ -39,60 +40,67 @@ public class Interact implements Listener {
 			
 			if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 
-				// Check if player has repair block in their main hand (mat)
 				
-				if(mat == Material.IRON_BLOCK || mat == Material.ANVIL || mat == Material.CHIPPED_ANVIL || mat == Material.DAMAGED_ANVIL)  {
+				// Get block and blockdata player is pointing at
+				Block b = player.getTargetBlock(null, 5);
+				Location loc = b.getLocation();
+				
+				
+				// Check if block is a broken anvil
+				if (b.getType() == Material.CHIPPED_ANVIL || b.getType() == Material.DAMAGED_ANVIL) {   //if(mat == Material.IRON_BLOCK || mat == Material.ANVIL || mat == Material.CHIPPED_ANVIL || mat == Material.DAMAGED_ANVIL)  {
 
-					// Get block and blockdata player is pointing at
-					Block b = player.getTargetBlock(null, 5);
-					Location loc = b.getLocation();
-					
-					
+					// Check if player has repair block in their main hand (mat)
+					if(mat == Material.IRON_BLOCK || mat == Material.ANVIL || mat == Material.CHIPPED_ANVIL || mat == Material.DAMAGED_ANVIL)  {
+						
 					// Get block data
 					BlockData data = b.getBlockData();
 					
-					if (b.getType() == Material.CHIPPED_ANVIL || b.getType() == Material.DAMAGED_ANVIL) {
-						
-						// Check if player is sneaking
-						if (!player.isSneaking()) {
+						if (b.getType() == Material.CHIPPED_ANVIL || b.getType() == Material.DAMAGED_ANVIL) {
 							
-							if (data instanceof Directional) {
+							// Check if player is sneaking
+							if (!player.isSneaking()) {
 								
-								// Get block face
-								BlockFace face = ((Directional) data).getFacing();
-								loc.getBlock().setType(Material.ANVIL); // Set anvil in target block
-								
-								// Get data for the new block
-								b = player.getTargetBlock(null, 5);
-								data = b.getBlockData();
-								
-								// Modify and set block data
-								((Directional) data).setFacing(face);
-								b.setBlockData(data);
-								
-							}
-							
-							// Check for permission to bypass price in "config.yml"
-							if (!player.hasPermission(plugin.getCustomConfig().getString("permissions.no-price"))) {
-								
-								player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount()-1); // This nice function clears 1 item in the main hand
-							}
-							
-							// Check for effect in "config.yml"
-							if (plugin.getCustomConfig().getBoolean("effects.enabled")) {
-								
-								// add 1 to Y to block location
-								Location locY1 = loc.clone().add(0, 1, 0);
-								
-								// Spawn cloud particle in anvil location
-								DustOptions dustOptions = new DustOptions(Color.fromRGB(69, 69, 69), 1);
-								player.spawnParticle(Particle.REDSTONE, locY1, 27, dustOptions);
+								if (data instanceof Directional) {
 									
-								System.out.println("Particles spawned for player: " + player.getName());
+									// Get block face
+									BlockFace face = ((Directional) data).getFacing();
+									loc.getBlock().setType(Material.ANVIL); // Set anvil in target block
+									
+									// Get data for the new block
+									b = player.getTargetBlock(null, 5);
+									data = b.getBlockData();
+									
+									// Modify and set block data
+									((Directional) data).setFacing(face);
+									b.setBlockData(data);
+									
+								}
+								
+								// Check for permission to bypass price in "config.yml"
+								if (!player.hasPermission(plugin.getCustomConfig().getString("permissions.no-price"))) {
+									
+									player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount()-1); // This nice function clears 1 item in the main hand
+								}
+								
+								// Check for effect in "config.yml"
+								if (plugin.getCustomConfig().getBoolean("effects.enabled")) {
+									
+									// add 1 to Y to block location
+									Location locY1 = loc.clone().add(0, 1, 0);
+									
+									// Spawn cloud particle in anvil location
+									DustOptions dustOptions = new DustOptions(Color.fromRGB(69,69,69), plugin.getCustomConfig().getInt("effects.size"));
+									player.spawnParticle(Particle.REDSTONE, locY1, plugin.getCustomConfig().getInt("effects.density"), dustOptions);
+										
+	
+									// Spawn sound in anvil location
+									//player.getWorld().playSound(loc, Sound.BLOCK_ANVIL_FALL, 3.0F, 0.5F);
+								}
+								event.setCancelled(true);
 							}
-							event.setCancelled(true);
 						}
 					}
+					
 				}
 			}
 		}
